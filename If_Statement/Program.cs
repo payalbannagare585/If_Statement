d0fe64a5a8a29aa8bf4d9b4853d87d1f833d88d9
Gaurav <Gaurav@LAPTOP-TREG306B>
2022-09-14 10:05:21 +0530

    Add project files.

---
+++ If_Statement/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            if (num>=0)
+            {
+                Console.WriteLine("number is positive");
+
+            }
+            else if (num<0)
+            {
+                Console.WriteLine("number is negative");
+
+            
+            }
+            else
+            {
+                Console.WriteLine("number is zero");
+            }
+
+        }
+    }
+}
